31c6b8aca80fd8098e6d170c905f07316a5e0dcb
cpu <cpuautomation1@gmail.com>
2019-09-29 00:22:44 -0400

    Angular commit

---
+++ src/Angular.java
@@ -0,0 +1,12 @@
+
+public class Angular {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Angular1");
+		System.out.println("Angular2");
+
+	}
+
+}
